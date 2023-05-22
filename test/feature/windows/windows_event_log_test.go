4ad9e1106661a204bda5050196379d7ae54f485b
Pooja Reddy Nathala <poojardy@amazon.com>
2023-05-22 12:01:45 -0400

    refactored code to validate the windows event log xml content

--- test/feature/windows/windows_event_log_test.go
+++ test/feature/windows/windows_event_log_test.go
@@ -3,10 +3,8 @@
 package windows
 
 import (
-	"fmt"
-	"github.com/aws/amazon-cloudwatch-agent-test/test/cloudwatchlogs"
+	"encoding/xml"
 	"log"
-	"strings"
 	"testing"
 	"time"
 
@@ -14,6 +12,76 @@ import (
 	"github.com/aws/amazon-cloudwatch-agent-test/internal/common"
 	"github.com/stretchr/testify/assert"
 )
+
+const (
+	configOutputPath = "/opt/aws/amazon-cloudwatch-agent/bin/config.json"
+	agentRuntime     = 20 * time.Second // default flush interval is 5 seconds
+)
+
+// Event is the event entry representation
+// Only the most common elements are processed, human-readable data is rendered in Message
+// More info on schema, if there will be need to add more:
+// https://docs.microsoft.com/en-us/windows/win32/wes/eventschema-elements
+type Event struct {
+	Source        Provider    `xml:"System>Provider"`
+	EventID       int         `xml:"System>EventID"`
+	Version       int         `xml:"System>Version"`
+	Level         int         `xml:"System>Level"`
+	Task          int         `xml:"System>Task"`
+	Opcode        int         `xml:"System>Opcode"`
+	Keywords      string      `xml:"System>Keywords"`
+	TimeCreated   TimeCreated `xml:"System>TimeCreated"`
+	EventRecordID int         `xml:"System>EventRecordID"`
+	Correlation   Correlation `xml:"System>Correlation"`
+	Execution     Execution   `xml:"System>Execution"`
+	Channel       string      `xml:"System>Channel"`
+	Computer      string      `xml:"System>Computer"`
+	Security      Security    `xml:"System>Security"`
+	UserData      UserData    `xml:"UserData"`
+	EventData     EventData   `xml:"EventData"`
+	Message       string
+	LevelText     string
+	TaskText      string
+	OpcodeText    string
+}
+
+// UserData Application-provided XML data
+type UserData struct {
+	InnerXML []byte `xml:",innerxml"`
+}
+
+// EventData Application-provided XML data
+type EventData struct {
+	InnerXML []byte `xml:",innerxml"`
+}
+
+// Provider is the Event provider information
+type Provider struct {
+	Name string `xml:"Name,attr"`
+}
+
+// Correlation is used for the event grouping
+type Correlation struct {
+	ActivityID        string `xml:"ActivityID,attr"`
+	RelatedActivityID string `xml:"RelatedActivityID,attr"`
+}
+
+// Execution Info for Event
+type Execution struct {
+	ProcessID   uint32 `xml:"ProcessID,attr"`
+	ThreadID    uint32 `xml:"ThreadID,attr"`
+	ProcessName string
+}
+
+// Security Data for Event
+type Security struct {
+	UserID string `xml:"UserID,attr"`
+}
+
+// TimeCreated field for Event
+type TimeCreated struct {
+	SystemTime string `xml:"SystemTime,attr"`
+}
 
 func TestWindowsEventLog(t *testing.T) {
 	log.Printf("Testing Windows Plugin")
@@ -25,37 +93,26 @@ func TestWindowsEventLog(t *testing.T) {
 	logStream := instanceId
 
 	start := time.Now()
-	common.CopyFile(cfgFilePath, cloudwatchlogs.configOutputPath)
+	common.CopyFile(cfgFilePath, configOutputPath)
 
-	common.StartAgent(cloudwatchlogs.configOutputPath, true)
+	common.StartAgent(configOutputPath, true)
 
 	// ensure that there is enough time from the "start" time and the first log line,
 	// so we don't miss it in the GetLogEvents call
-	time.Sleep(cloudwatchlogs.agentRuntime)
+	time.Sleep(agentRuntime)
 	t.Log("Writing logs from windows event log plugin")
-	time.Sleep(cloudwatchlogs.agentRuntime)
+	time.Sleep(agentRuntime)
 	common.StopAgent()
-
-	lines := []string{
-		fmt.Sprintf("{\"Metric\": \"%s\"}", strings.Repeat("12345", 10)),
-		fmt.Sprintf("{\"Metric\": \"%s\"}", strings.Repeat("09876", 10)),
-		fmt.Sprintf("{\"Metric\": \"%s\"}", strings.Repeat("1234567890", 10)),
-	}
 
 	end := time.Now()
 
 	ok, err := awsservice.ValidateLogs(logGroup, logStream, &start, &end, func(logs []string) bool {
-		log.Printf("logs length: %s ", len(logs))
-		if len(logs) != len(lines) {
-			return false
-		}
+		log.Printf("logs length: %d ", len(logs))
 
 		for i := 0; i < len(logs); i++ {
-			log.Printf("lines[%s] is %s", i, lines[i])
-			log.Printf("logs[%s] is %s", i, logs[i])
-			expected := strings.ReplaceAll(lines[i], "'", "\"")
-			actual := strings.ReplaceAll(logs[i], "'", "\"")
-			if expected != actual {
+			log.Printf("logs[%d] is %s", i, logs[i])
+			err := validateXML(logs[i])
+			if err != nil {
 				return false
 			}
 		}
@@ -64,4 +121,18 @@ func TestWindowsEventLog(t *testing.T) {
 	})
 	assert.NoError(t, err)
 	assert.True(t, ok)
+}
+
+func TestXML(t *testing.T) {
+	log.Printf("Testing xml Plugin")
+	logs := []string{
+		"<Event xmlns='http://schemas.microsoft.com/win/2004/08/events/event'><System><Provider Name='Service Control Manager' Guid='{555908d1-a6d7-4695-8e1e-26931d2012f4}' EventSourceName='Service Control Manager'/><EventID Qualifiers='16384'>7036</EventID><Version>0</Version><Level>4</Level><Task>0</Task><Opcode>0</Opcode><Keywords>0x8080000000000000</Keywords><TimeCreated SystemTime='2023-05-22T14:43:53.1670895Z'/><EventRecordID>64199</EventRecordID><Correlation/><Execution ProcessID='776' ThreadID='5788'/><Channel>System</Channel><Computer>EC2AMAZ-25GMQOK</Computer><Security/></System><EventData><Data Name='param1'>AppX Deployment Service (AppXSVC)</Data><Data Name='param2'>running</Data><Binary>41007000700058005300760063002F0034000000</Binary></EventData><RenderingInfo Culture='en-US'><Message>The AppX Deployment Service (AppXSVC) service entered the running state.</Message><Level>Information</Level><Task></Task><Opcode></Opcode><Channel></Channel><Provider>Microsoft-Windows-Service Control Manager</Provider><Keywords><Keyword>Classic</Keyword></Keywords></RenderingInfo></Event>",
+	}
+	assert.True(t, validateXML(logs[0]) == nil)
+}
+
+func validateXML(xmlString string) error {
+	var event Event
+	err := xml.Unmarshal([]byte(xmlString), &event)
+	return err
 }

28b2c5607cc2b5c2c3b26613399c6e7abd62103e
Pooja Reddy Nathala <poojardy@amazon.com>
2023-05-30 12:41:20 -0400

    added influxdata module and refactored code

--- test/feature/windows/windows_event_log_test.go
+++ test/feature/windows/windows_event_log_test.go
@@ -9,9 +9,9 @@ import (
 	"time"
 
 	"github.com/aws/amazon-cloudwatch-agent-test/environment"
-
 	"github.com/aws/amazon-cloudwatch-agent-test/internal/awsservice"
 	"github.com/aws/amazon-cloudwatch-agent-test/internal/common"
+	"github.com/influxdata/telegraf/plugins/inputs/win_eventlog"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -19,71 +19,6 @@ const (
 	configOutputPath    = "C:\\ProgramData\\Amazon\\AmazonCloudWatchAgent\\amazon-cloudwatch-agent.json"
 	agentWindowsRuntime = 3 * time.Minute
 )
-
-// Event is the event entry representation
-// Only the most common elements are processed, human-readable data is rendered in Message
-// More info on schema, if there will be need to add more:
-// https://docs.microsoft.com/en-us/windows/win32/wes/eventschema-elements
-type Event struct {
-	Source        Provider    `xml:"System>Provider"`
-	EventID       int         `xml:"System>EventID"`
-	Version       int         `xml:"System>Version"`
-	Level         int         `xml:"System>Level"`
-	Task          int         `xml:"System>Task"`
-	Opcode        int         `xml:"System>Opcode"`
-	Keywords      string      `xml:"System>Keywords"`
-	TimeCreated   TimeCreated `xml:"System>TimeCreated"`
-	EventRecordID int         `xml:"System>EventRecordID"`
-	Correlation   Correlation `xml:"System>Correlation"`
-	Execution     Execution   `xml:"System>Execution"`
-	Channel       string      `xml:"System>Channel"`
-	Computer      string      `xml:"System>Computer"`
-	Security      Security    `xml:"System>Security"`
-	UserData      UserData    `xml:"UserData"`
-	EventData     EventData   `xml:"EventData"`
-	Message       string
-	LevelText     string
-	TaskText      string
-	OpcodeText    string
-}
-
-// UserData Application-provided XML data
-type UserData struct {
-	InnerXML []byte `xml:",innerxml"`
-}
-
-// EventData Application-provided XML data
-type EventData struct {
-	InnerXML []byte `xml:",innerxml"`
-}
-
-// Provider is the Event provider information
-type Provider struct {
-	Name string `xml:"Name,attr"`
-}
-
-// Correlation is used for the event grouping
-type Correlation struct {
-	ActivityID        string `xml:"ActivityID,attr"`
-	RelatedActivityID string `xml:"RelatedActivityID,attr"`
-}
-
-// Execution Info for Event
-type Execution struct {
-	ProcessID   uint32 `xml:"ProcessID,attr"`
-	ThreadID    uint32 `xml:"ThreadID,attr"`
-	ProcessName string
-}
-
-// Security Data for Event
-type Security struct {
-	UserID string `xml:"UserID,attr"`
-}
-
-// TimeCreated field for Event
-type TimeCreated struct {
-	SystemTime string `xml:"SystemTime,attr"`
-}
 
 var envMetaDataStrings = &(environment.MetaDataStrings{})
 
@@ -139,7 +74,7 @@ func TestXML(t *testing.T) {
 }
 
 func validateXML(xmlString string) error {
-	var event Event
+	var event win_eventlog.Event
 	err := xml.Unmarshal([]byte(xmlString), &event)
 	return err
 }
